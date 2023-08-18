0f4544db2b3a2a2369eee3964d65c440cac33ee6
Mayesha Rashid <75426300+mrashid5919@users.noreply.github.com>
2023-08-18 20:29:40 +0600

    Update 1905103.cpp

--- Offline_3(Max Cut)/1905103.cpp
+++ Offline_3(Max Cut)/1905103.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #define fastio ios::sync_with_stdio(0);cin.tie(0)
 
-long long n,m,pos[100005],mx,mxa,mxb;
+long long n,m,pos[100005],mx,mxa,mxb,mn,mna,mnb;
 vector<pair<long long,pair<long long,long long> > > edges;
 vector<pair<long long,long long> > adj[100005];
 
@@ -94,6 +94,86 @@ long long greedyMaxCut()
     return val;
 }
 
+long long semiGreedyMaxCut()
+{
+    for(long long i=0;i<n;i++)
+        pos[i]=0;
+    vector<long long> setX,setY;
+    long long alphaInt=rand()%101;
+    float alpha=alphaInt*0.01;
+    float miu=mn+alpha*(mx-mn);
+    vector<pair<long long,long long> > RCLe;
+    for(long long i=0;i<m;i++)
+    {
+        if((float)edges[i].second.second>=miu)
+            RCLe.push_back({edges[i].first,edges[i].second.first});
+    }
+    //cout<<"here"<<"\n";
+    long long idx=rand()%RCLe.size();
+    setX.push_back(RCLe[idx].first);
+    setY.push_back(RCLe[idx].second);
+    pos[RCLe[idx].first]=1;
+    pos[RCLe[idx].second]=2;
+    long long mxSigma,mnSigma;
+    while(setX.size()+setY.size()!=n)
+    {
+        mxSigma=LLONG_MIN,mnSigma=LLONG_MAX;
+        vector<pair<long long,pair<long long,long long> > > mxSigmaInfo;
+        for(long long i=0;i<n;i++)
+        {
+            if(pos[i]==0)
+            {
+                long long curX=0,curY=0;
+                for(long long k=0;k<adj[i].size();k++)
+                {
+                    if(pos[adj[i][k].first]==1)
+                        curY+=adj[i][k].second;
+                    else if(pos[adj[i][k].first]==2)
+                        curX+=adj[i][k].second;
+                }
+                if(curY>=curX)
+                    mxSigmaInfo.push_back({curY,{i,2}}); //2 indicates wanting to assign it to setY
+                else
+                    mxSigmaInfo.push_back({curX,{i,1}}); //1 indicates wanting to assign it to setX
+                if(max(curX,curY)>mxSigma)
+                {
+                    mxSigma=max(curX,curY);
+                    /*if(curX>=curY)
+                        mxSigmaPos=2;
+                    else
+                        mxSigmaPos=1;
+                    mxV=i;*/
+                }
+                if(min(curX,curY)<mnSigma)
+                    mnSigma=min(curX,curY);
+            }
+        }
+        
+        miu=mnSigma+alpha*(mxSigma-mnSigma);
+        vector<pair<long long,long long> > RCLv;
+        for(long long i=0;i<mxSigmaInfo.size();i++)
+        {
+            if((float)mxSigmaInfo[i].first>=miu)
+                RCLv.push_back(mxSigmaInfo[i].second); //storing the vertex number and the set we desire to insert it into
+        }
+        long long idx=rand()%RCLv.size();
+        if(RCLv[idx].second==1)
+        {
+            setX.push_back(RCLv[idx].first);
+        }
+        else
+            setY.push_back(RCLv[idx].first);
+        pos[RCLv[idx].first]=RCLv[idx].second;
+    }
+    long long val=0;
+    for(long long i=0;i<m;i++)
+    {
+        if(pos[edges[i].first]!=pos[edges[i].second.first])
+            val+=edges[i].second.second;
+    }
+    return val;
+}
+
 int main()
 {
     fastio;
@@ -107,12 +187,15 @@ int main()
         adj[b-1].push_back({a-1,w});
         edges.push_back({a-1,{b-1,w}});
 
-        //Identifying the most heavy edge, required for greedy
+        //Identifying the most heavy edge and most light edge, required for greedy, semigreedy
         if(i==0)
         {
             mx=w;
             mxa=a-1;
             mxb=b-1;
+            mn=w;
+            mna=a-1;
+            mnb=b-1;
         }
         else if(w>mx)
         {
@@ -120,9 +203,17 @@ int main()
             mxa=a-1;
             mxb=b-1;
         }
+        else if(w<mn)
+        {
+            mn=w;
+            mna=a-1;
+            mnb=b-1;
+        }
     }
+    //cout<<"here\n";
     cout<<"Randomized: "<<randomMaxCut()<<"\n";
     cout<<"Greedy: "<<greedyMaxCut()<<"\n";
+    cout<<"Semigreedy: "<<semiGreedyMaxCut()<<"\n";
     //for(i=0;i<n;i++)
         //cout<<pos[i]<<" ";
 }
